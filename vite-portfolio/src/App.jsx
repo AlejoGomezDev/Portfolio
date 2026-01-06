1d71646131f876946a42e0847139106ca8a99ed8
Alejo Gomez <alejongomez@gmail.com>
2026-01-06 00:43:51 -0300

    first commit

---
+++ vite-portfolio/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+
+     <div className="container min-w-full max-h-screen min-h-screen text-white">
+      
+     
+
+      
+
+    </div>
+ 
+  )
+}
+
+export default App

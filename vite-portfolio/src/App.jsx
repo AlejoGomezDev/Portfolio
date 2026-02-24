ac3c0a8d20932a112b02096ffb5eecd53bd1fdb0
Alejo Gomez <alejongomez@gmail.com>
2026-02-24 00:30:22 -0300

    Correcciones navbar y fondo difuminado

--- vite-portfolio/src/App.jsx
+++ vite-portfolio/src/App.jsx
@@ -3,31 +3,55 @@ import { Navbar } from "./components/navbar/navbar";
 import { SECTIONS } from "./data/sections";
 import { useEffect, useRef, useState } from "react";
 import { EllipsisIcon } from "lucide-react";
+import { useScroll } from "framer-motion";
+
 function App() {
   const [selectedSection, setSelectedSection] = useState(SECTIONS[0].id);
   const [isNavbarHidden, setIsNavbarHidden] = useState(false);
-  const [canShowNavbar, setCanShowNavbar] = useState(true);
+  const lastScrollTop = useRef(0);
+
+  const { scrollY } = useScroll();
+
+useEffect(() => {
+  const unsubscribe = scrollY.on("change", (current) => {
+    const scrollingDown = current > lastScrollTop.current;
+
+    if (current <= 20) {
+      setIsNavbarHidden(false);
+    } else if (scrollingDown ) {
+      setIsNavbarHidden(true);
+    } 
+
+    lastScrollTop.current = current;
+  });
+
+  return unsubscribe;
+}, [scrollY]);
 
   const handleNavbarMouseLeave = () => {
     if (selectedSection !== SECTIONS[0].id) {
       setTimeout(() => {
         setIsNavbarHidden(true);
       }, 100);
-    }else{
-      setIsNavbarHidden(false)
+    } else {
+      setIsNavbarHidden(false);
     }
   };
 
   const handleNavbarClick = (e, id) => {
     e.preventDefault();
+
     const element = document.getElementById(id);
     if (element) {
       element.scrollIntoView({ behavior: "smooth" });
     }
-    setSelectedSection(id);
-    setIsNavbarHidden(true);
-    setCanShowNavbar(false);
-    window.history.pushState({}, "", `#${id}`);
+
+    //Solo ocultar si no es la misma seccion
+    if(id !== selectedSection){
+      setIsNavbarHidden(true);
+      setSelectedSection(id);
+      window.history.pushState({}, "", `#${id}`);
+    }
   };
 
   const Logo = () => {
@@ -48,17 +72,14 @@ function App() {
     );
   };
 
-  useEffect(() => {
-    console.log("escondida", isNavbarHidden);
-  }, [isNavbarHidden]);
 
   return (
-    <div className="min-w-full h-screen text-gray-200 flex flex-col overflow-hidden font-inter scroll-smooth">
+    <div className="min-w-full h-full text-gray-200 flex flex-col font-inter scroll-smooth app-background">
       {/* Header con el navbar */}
-      <header>
+      <header className="relative">
         <Navbar
           sections={SECTIONS}
-          navbarClassName={`w-[80%] absolute top-2 left-0 right-0 m-auto bg-gray-700/20 rounded-full shrink-0 backdrop-blur-xl border border-gray-800 h-[5em] font-normal transition-all duration-300 transform ${
+          navbarClassName={`w-[80%] fixed top-2 left-0 right-0 m-auto bg-gray-700/40 rounded-full shrink-0 backdrop-blur-3xl border border-gray-800 h-[5em] font-normal transition-all duration-300 transform ${
             isNavbarHidden
               ? "-translate-y-[120%] opacity-0 pointer-events-none"
               : "translate-y-0 opacity-100"
@@ -76,34 +97,31 @@ function App() {
         />
 
         <div
-          className={`absolute top-0 h-[3em] w-[15%]  m-auto left-0 right-0 transition-all duration-300  ${
+          className={`fixed top-0 h-[1.5em]  w-[15%]  m-auto left-0 right-0 transition-all duration-300  ${
             isNavbarHidden
               ? "opacity-100 translate-y-0"
-              : "opacity-0 translate-y-full pointer-events-none"
+              : "opacity-100 -translate-y-20 pointer-events-none"
           }`}
           onMouseEnter={() => {
-            if(canShowNavbar && isNavbarHidden){
-              setIsNavbarHidden(false);
-            }
-          }}
-
-          onMouseLeave={()=>{
-            setCanShowNavbar(true)
+            setIsNavbarHidden(false)
           }}
         >
-          <div className="flex items-center text-center h-[2em] cursor-default justify-center rounded-full border bg-gray-700/20 border-gray-800 shrink-0 backdrop-blur-xl mt-4">
+          <div className="flex items-center text-center h-full cursor-default justify-center rounded-full border bg-gray-700/40 border-gray-800 shrink-0 backdrop-blur-3xl mt-1">
             <EllipsisIcon className="h-5" />
           </div>
         </div>
       </header>
-      <main className="h-screen flex-1 overflow-y-auto">
+
+      <main
+        className="flex flex-col "
+      >
         {SECTIONS.map((section) => {
           const Component = section.componentToRender;
           return (
             <section
               key={section.id}
               id={section.id}
-              className={`min-h-screen ${section.bgColor}`}
+              className={`w-[80%] mx-auto min-h-dvh `}
             >
               <h2 className="sr-only">{section.label}</h2>
               <Component />
